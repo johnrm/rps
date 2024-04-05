29b10cdf96dc0dbdd75db2c7ddbd3daae330184d
johnrm <notjohnrm@gmail.com>
2024-04-05 10:22:26 +0000

    Add and test computer guess

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,3 +8,14 @@ let computerScore = 0;
 let userScore = 0;
 let computerRPS = "";
 let userRPS = "";
+
+console.log (computerGuess ());
+
+/**
+ * Computer guess
+ */
+function computerGuess() {
+    let guess = (Math.floor(Math.random() * 3));
+    console.log("computer: " + guess);
+    return (rpsArray[guess]);
+}
