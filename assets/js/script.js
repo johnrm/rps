b07afad212241cdd8e6dcf7d89f5a638d965493e
johnrm <notjohnrm@gmail.com>
2024-04-05 10:18:28 +0000

    Add JS array plus needed variables

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+let rpsArray = ['rock','scissors','paper'];
+
+/**
+ * Clear the decks
+ * Reset game counters
+ */
+let computerScore = 0;
+let userScore = 0;
+let computerRPS = "";
+let userRPS = "";
